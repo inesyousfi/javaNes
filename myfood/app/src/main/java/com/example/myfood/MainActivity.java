9447154148b2c9bfec4b64e7d63db30d977e1340
inesyousfi <101816936+inesyousfi@users.noreply.github.com>
2022-11-10 10:52:51 +0300

    Add files via upload

---
+++ myfood/app/src/main/java/com/example/myfood/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayList<String> name =new ArrayList();
+        name.add("basma");,name.add("ines"),name.add()
+
+        ArrayList myAdapter =new ArrayList(this,R.layout.activity_main,)
+
+
+    }
+}
